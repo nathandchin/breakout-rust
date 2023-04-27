ec51bdf781ad928b37e55ae04dbf8c0cec53a936
Nathan Chin <nathanchin@nathanchin.com>
2023-04-27 00:29:14 -0400

    Rename and cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,18 @@ use paddle::Paddle;
 mod ball;
 use ball::Ball;
 
-const WIDTH: i32 = 800;
+const WIDTH: i32 = 1000;
 const HEIGHT: i32 = 1600;
 
 fn main() {
     let (mut rl, thread) = raylib::init().size(WIDTH, HEIGHT).title("Breakout").build();
-    let mut p = Paddle::new(
+    let mut paddle = Paddle::new(
         WIDTH as f32 / 8.0,
         HEIGHT as f32 / 40.0,
         WIDTH as f32 / 2.0,
         HEIGHT as f32 * (4.0 / 5.0),
     );
-    let mut b = Ball::new(WIDTH as f32 / 2.0, HEIGHT as f32 / 2.0);
+    let mut ball = Ball::new(WIDTH as f32 / 2.0, HEIGHT as f32 / 2.0);
     let walls = vec![
         Rectangle::new(0.0, 0.0, WIDTH as f32, 0.0), // Top
         Rectangle::new(0.0, HEIGHT as f32, WIDTH as f32, 0.0), // Bottom
@@ -26,17 +26,15 @@ fn main() {
 
     while !rl.window_should_close() {
         let mut colliders = walls.clone();
-        colliders.push(p.rect.clone());
+        colliders.push(paddle.rect.clone());
 
         let mut d = rl.begin_drawing(&thread);
 
-        // dbg!(&walls);
-
-        p.update(&mut d);
-        b.update(&colliders);
+        paddle.update(&mut d);
+        ball.update(&colliders);
 
         d.clear_background(Color::BLACK);
-        p.draw(&mut d);
-        b.draw(&mut d);
+        paddle.draw(&mut d);
+        ball.draw(&mut d);
     }
 }
